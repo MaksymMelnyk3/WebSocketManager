8bdd537cc4da45c67c60132e3fe90af4b4ffc7b8
Anastasiia Spiridonova <anastasiia_spiridonova@epam.com>
2023-01-21 12:38:34 +0200

    game UI

--- Sources/WebSocketManager/SocketManager/Models/MapState.swift
+++ Sources/WebSocketManager/SocketManager/Models/MapState.swift
@@ -9,10 +9,10 @@ import Foundation
 
 
 public struct MapState {
-    let tick: Int
-    let lastResivedTick: Int
-    let food: [Food]
-    let cell: [Cell]
+    public let tick: Int
+    public let lastResivedTick: Int
+    public let food: [Food]
+    public let cell: [Cell]
     
     init(food: [Food], cell: [Cell], tick: Int, lastResivedTick: Int) {
         self.food = food
@@ -23,35 +23,35 @@ public struct MapState {
 }
 
 public struct Food {
-    var id: String
-    var isNew: Bool
-    var position: Coordinates
-    var del: Bool
+    public var id: String
+    public var isNew: Bool
+    public var position: Coordinates
+    public var del: Bool
 }
 
 public struct Cell {
-    var id: String
-    var isNew: Bool
-    var player: String
-    var mass: Double
-    var canSplit: Bool
-    var radius: Double
-    var velocity: Coordinates
-    var speed: Double
-    var direction: Coordinates
-    var availableEnergy, eatEfficiency: Double
-    var maxSpeed: Double
-    var power, volatilization: Double
-    var mergeTimer: Int
-    var canMerge: Bool
-    var position: Coordinates
-    var own: Bool
-    var del: Bool
+    public var id: String
+    public var isNew: Bool
+    public var player: String
+    public var mass: Double
+    public var canSplit: Bool
+    public var radius: Double
+    public var velocity: Coordinates
+    public var speed: Double
+    public var direction: Coordinates
+    public var availableEnergy, eatEfficiency: Double
+    public var maxSpeed: Double
+    public var power, volatilization: Double
+    public var mergeTimer: Int
+    public var canMerge: Bool
+    public var position: Coordinates
+    public var own: Bool
+    public var del: Bool
 }
 
 public struct Coordinates {
-    let x: Double
-    let y: Double
+    public let x: Double
+    public let y: Double
 }
 
 
--- Sources/WebSocketManager/SocketManager/WebSocketManager.swift
+++ Sources/WebSocketManager/SocketManager/WebSocketManager.swift
@@ -1,8 +1,23 @@
 import Foundation
 import Starscream
 
-@available(iOS 13.0, *)
+public protocol WebSocketManagerDelegate: AnyObject {
+    func webSocketManager(didConnect player: String)
+    
+    func webSocketManager(didReceive gameConfig: GameConfig)
+    
+    func webSocketManager(didReceive newCells: [Cell])
+    func webSocketManager(didRemove cells: [Cell])
+    func webSocketManager(didUpdate cells: [Cell])
+    
+    func webSocketManager(didReceive newFood: [Food])
+    func webSocketManager(didRemove food: [Food])
+}
+
+
 public class WebSocketManager {
+    public weak var delegate: WebSocketManagerDelegate?
+    
     private let socket = WebSocket(request: URLRequest(url: URL(string: "http://45.77.67.171/")!))
     
     private var gameState = GameState()
@@ -20,7 +35,7 @@ public class WebSocketManager {
       socket.delegate = nil
     }
     
-    public func connect(roomName: String, watch: Bool) {
+    public func connect(roomName: String, watch: Bool = false) {
         socket.connect()
         DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
         let connectModel = ConnectModel(data: ConnectDataModel(roomName: roomName, watch: watch))
@@ -52,17 +67,21 @@ public class WebSocketManager {
             switch gameData.key {
             case Keys.connected.rawValue:
                 let connectData: ConnectedModel? = self?.decode(data: data)
-                self?.gameState.playerId = connectData?.data?.playerID
+                guard let playerId = connectData?.data?.playerID else { return }
+                self?.gameState.setPlayer(playerId)
+                self?.delegate?.webSocketManager(didConnect: playerId)
             case Keys.config.rawValue:
                 guard let configData: GameConfigDTO = self?.decode(data: data) else { return }
-                self?.cellLogic.configure(gameConfig: GameConfig(with: configData.data))
+                let gameConfig = GameConfig(with: configData.data)
+                self?.cellLogic.configure(gameConfig: gameConfig)
+                self?.delegate?.webSocketManager(didReceive: gameConfig)
             case Keys.data.rawValue:
                 guard let gameData: GameDataModel = self?.decode(data: data) else { return }
                 self?.dataReceived(tick: gameData.data?.tick ?? 0)
                 
-                self?.gameState.ticksOperation(gameData: gameData)
-                self?.gameState.cellsOperation(gameData: gameData)
-                self?.gameState.foodsOperation(gameData: gameData)
+                self?.ticksOperation(gameData: gameData)
+                self?.cellsOperation(gameData: gameData)
+                self?.foodsOperation(gameData: gameData)
                 
                 self?.playerAction()
                 
@@ -134,6 +153,209 @@ public class WebSocketManager {
     }
 }
 
+private extension WebSocketManager {
+    
+    func ticksOperation(gameData: GameDataModel) {
+        if let tick = gameData.data?.tick {
+            self.gameState.tick = tick
+        }
+        
+        if let lastTick = gameData.data?.lastReceivedTick {
+            self.gameState.lastTick = lastTick
+        }
+    }
+    
+    func cellsOperation(gameData: GameDataModel) {
+        var cells = self.gameState.cell
+        if let deeltedCells = gameData.data?.cells?.filter({ $0.del ?? false }) {
+            deeltedCells.forEach { cell in
+                cells.removeAll(where: { $0.id == cell.id })
+            }
+            delegate?.webSocketManager(didRemove: deeltedCells.map({ Cell($0, playerId: gameState.playerId) }))
+        }
+        
+        if let updatedCells = gameData.data?.cells?.filter({ cell in
+            (cell.isNew == false || cell.isNew == nil) &&
+            (cell.del == false ||  cell.del == nil)
+        }) {
+            updatedCells.forEach { cell in
+                if let index = cells.firstIndex(where: { cell.id == $0.id } ) {
+                    var cellToUpdate = cells[index]
+                    cellToUpdate.update(with: cell)
+                    cells[index] = cellToUpdate
+                }
+            }
+            delegate?.webSocketManager(didUpdate: cells)
+        }
+        
+        if let newCells = gameData.data?.cells?.filter({ $0.isNew ?? false }) {
+            newCells.forEach { cell in
+                if !cells.contains(where: { $0.id == cell.id }) {
+                    cells.append(Cell(cell, playerId: gameState.playerId))
+                }
+            }
+            delegate?.webSocketManager(didReceive: newCells.map({ Cell($0, playerId: gameState.playerId) }))
+        }
+        self.gameState.updateCells(cells)
+    }
+    
+    func foodsOperation(gameData: GameDataModel) {
+        var foods = self.gameState.food
+        if let deeltedFoods = gameData.data?.food?.filter({ $0.del ?? false }) {
+            deeltedFoods.forEach { food in
+                foods.removeAll(where: { $0.id == food.id })
+            }
+            delegate?.webSocketManager(didRemove: deeltedFoods.map(Food.init))
+        }
+        
+        if let updatedFoods = gameData.data?.food?.filter({ food in
+            (food.isNew == false || food.isNew == nil) &&
+            (food.del == false ||  food.del == nil)
+        }) {
+            updatedFoods.forEach { food in
+                if let index = foods.firstIndex(where: { food.id == $0.id } ) {
+                    var foodToUpdate = foods[index]
+                    
+                    foodToUpdate.update(with: food)
+                    
+                    foods[index] = foodToUpdate
+                }
+            }
+        }
+        
+        if let newFoods = gameData.data?.food?.filter({ $0.isNew ?? false }) {
+            newFoods.forEach { food in
+                if !foods.contains(where: { $0.id == food.id }) {
+                    foods.append(Food(food)) }
+            }
+            delegate?.webSocketManager(didReceive: newFoods.map(Food.init))
+        }
+        
+        self.gameState.updateFood(foods)
+    }
+    
+}
+
+private extension Cell {
+    init(_ cell: CellDTO, playerId: String) {
+        id = cell.id
+        isNew = cell.isNew ?? false
+        player = cell.player ?? ""
+        mass = cell.mass ?? 0.0
+        canSplit = cell.canSplit ?? false
+        radius = cell.radius ?? 0.0
+        if let velocityDTO = cell.velocity {
+            velocity = Coordinates(x: velocityDTO.x, y: velocityDTO.y)
+        } else {
+            velocity = Coordinates(x: 0.0, y: 0.0)
+        }
+        speed = cell.speed ?? 0.0
+        if let directionDTO = cell.direction {
+            direction = Coordinates(x: directionDTO.x, y: directionDTO.y)
+        } else {
+            direction = Coordinates(x: 0.0, y: 0.0)
+        }
+        availableEnergy = cell.availableEnergy ?? 0.0
+        eatEfficiency = cell.eatEfficiency ?? 0.0
+        maxSpeed = cell.maxSpeed ?? 0.0
+        power = cell.power ?? 0.0
+        volatilization = cell.volatilization ?? 0.0
+        mergeTimer = cell.mergeTimer ?? 0
+        canMerge = cell.canMerge ?? false
+        if let positionDTO = cell.position {
+            position = Coordinates(x: positionDTO.x, y: positionDTO.y)
+        } else {
+            position = Coordinates(x: 0.0, y: 0.0)
+        }
+        if let own = cell.own {
+            self.own = own
+        } else {
+            own = cell.player == playerId
+        }
+        del = cell.del ?? false
+    }
+    
+    mutating func update(with cell: CellDTO) {
+        guard cell.id == id else {
+            return
+        }
+        if let canMerge = cell.canMerge {
+            self.canMerge = canMerge
+        }
+        if let player = cell.player {
+            self.player = player
+        }
+        if let mass = cell.mass {
+            self.mass = mass
+        }
+        if let canSplit = cell.canSplit {
+            self.canSplit = canSplit
+        }
+        if let radius = cell.radius {
+            self.radius = radius
+        }
+        if let velocity = cell.velocity {
+            self.velocity = Coordinates(x: velocity.x, y: velocity.y)
+        }
+        if let speed = cell.speed {
+            self.speed = speed
+        }
+        if let direction = cell.direction {
+            self.direction = Coordinates(x: direction.x, y: direction.y)
+        }
+        if let availableEnergy = cell.availableEnergy {
+            self.availableEnergy = availableEnergy
+        }
+        if let eatEfficiency = cell.eatEfficiency {
+            self.eatEfficiency = eatEfficiency
+        }
+        if let maxSpeed = cell.maxSpeed {
+            self.maxSpeed = maxSpeed
+        }
+        if let power = cell.power {
+            self.power = power
+        }
+        if let volatilization = cell.volatilization {
+            self.volatilization = volatilization
+        }
+        if let mergeTimer = cell.mergeTimer {
+            self.mergeTimer = mergeTimer
+        }
+        if let position = cell.position {
+            self.position = Coordinates(x: position.x, y: position.y)
+        }
+        if let own = cell.own {
+            self.own = own
+        }
+        if let del = cell.del {
+            self.del = del
+        }
+    }
+}
+
+private extension Food {
+    init(_ foodData: FoodDataDTO) {
+        id = foodData.id
+        isNew = foodData.isNew ?? false
+        position = Coordinates(x: foodData.position?.x ?? 0.0, y: foodData.position?.y ?? 0.0)
+        del = foodData.del ?? false
+    }
+    
+    mutating func update(with foodData: FoodDataDTO) {
+        guard id == foodData.id else { return }
+        if let isNew = foodData.isNew {
+            self.isNew = isNew
+        }
+        if let position = foodData.position {
+            self.position = Coordinates(x: position.x, y: position.y)
+        }
+        if let del = foodData.del {
+            self.del = del
+        }
+    }
+}
+
+
 private extension GameConfig {
     init(with dataConfigDTO: DataConfigDTO) {
         tickTime = dataConfigDTO.tickTime
@@ -168,3 +390,4 @@ private extension GameConfig {
         food = FoodConfig(mass: dataConfigDTO.food.mass)
     }
 }
+
